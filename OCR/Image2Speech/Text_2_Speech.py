ed79efba078dc11b3dc26538de36326241dbd6bd
Sarthak Pratik <66137271+mariner99@users.noreply.github.com>
2021-02-25 00:13:22 +0530

    Adding Comments ;)

--- OCR/Image2Speech/Text_2_Speech.py
+++ OCR/Image2Speech/Text_2_Speech.py
@@ -1,22 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+#For playing the audio without saving any file, gTTS requires a file to be saved.
 import pyttsx3
 
-
-# In[2]:
-
-
+# Function for Text to Speech
 def t2s(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
 
-
-# In[ ]:
 
 
 
